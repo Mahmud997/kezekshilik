b7a941d5061a0b2ae29e58ffab498e91f78af08e
Mahmud997 <mahmudjon343@gmail.com>
2026-09-09 20:17:12 +0600

    Add Firebase configuration to config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCTYEjm5FUGmbWiP7_Oylx9gYT-LZTCGqA",
+  authDomain: "school-journal-274ea.firebaseapp.com",
+  projectId: "school-journal-274ea",
+  storageBucket: "school-journal-274ea.firebasestorage.app",
+  messagingSenderId: "528767094233",
+  appId: "1:528767094233:web:f921456d55b0c49bf9ddc8"
+};
